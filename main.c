188a3f0bd2531e596744def5bba7c3c8e0a3307f
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-08-07 14:43:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -15,10 +15,10 @@
 
 #define DS 78   // training/data samples (13 floats per sample 12 input 1 output)
 #define DSS 1014// dataset size (num of 4-byte floats)
-#define EPOCHS 333333333
 #define DEBUG 1
 f32 dataset[DSS];
 network net;
+uint EPOCHS = 333333333;
 
 uint64_t microtime()
 {
@@ -126,6 +126,7 @@ int main()
     
     // learning rate decay
     // setLearningRate(&net, 0.1f);
+    // EPOCHS = 66000;
     // const f32 lr_rr = 0.09999f / (f32)EPOCHS; // learning rate _ reduction rate
 
     // train network
